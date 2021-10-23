736df8d0d7a574cc4602533ec3fe45e5762cbd70
Mohamed Belaid <medbbelaid@gmail.com>
2021-10-23 19:13:32 +0200

    Add dbjs file

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const github = {
+    baseUrl: "https://api.github.com/graphql",
+    username: "medBelaid",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: "bearer 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"
+    }
+}
+
+export default github
